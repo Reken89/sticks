4b5fad0fa375cbf08a781958157b9def4c291c7d
Administrator <Administrator@ADMIN.domenfo.com>
2024-02-12 09:18:19 +0300

    Setting up index

--- index.php
+++ index.php
@@ -1,48 +1,5 @@
-<!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        
-        ?>
-        <form id="FormOne" method="post">
-            <img src="stick.jpg" />
-            <img src="stick.jpg" />
-            <img src="stick.jpg" />
-            </br>
-            <input type="text" name="FormOne" required placeholder="Количество палочек">
-            <button type="button" id="FormOne" class="btn btn-primary">Взять</button>
-        </form>
-        </br>
-        <form id="FormTwo" method="post">
-            <img src="stick.jpg" />
-            <img src="stick.jpg" />
-            <img src="stick.jpg" />
-            <img src="stick.jpg" />
-            <img src="stick.jpg" />
-            </br>
-            <input type="text" name="FormTwo" required placeholder="Количество палочек">
-            <button type="button" id="FormTwo" class="btn btn-primary">Взять</button>
-        </form>
-        </br>
-        <form id="FormThree" method="post">
-            <img src="stick.jpg" />
-            <img src="stick.jpg" />
-            <img src="stick.jpg" />
-            <img src="stick.jpg" />
-            <img src="stick.jpg" />
-            <img src="stick.jpg" />
-            <img src="stick.jpg" />
-            </br>
-            <input type="text" name="FormThree" required placeholder="Количество палочек">
-            <button type="button" id="FormThree" class="btn btn-primary">Взять</button>
-        </form>
-    </body>
-</html>
+<?php
+
+session_start();
+require_once __DIR__ . '/vendor/autoload.php';
+require_once 'src/routes.php';
